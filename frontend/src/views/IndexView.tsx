9ebb598f5b713150806eb071421214a7f45cbbfa
Steven <93779447+eplxy@users.noreply.github.com>
2025-10-08 18:37:11 -0400

    Hide title on search

--- frontend/src/views/IndexView.tsx
+++ frontend/src/views/IndexView.tsx
@@ -23,12 +23,14 @@ export default function IndexView() {
 
   return (
     <Box sx={{ p: 3 }}>
-      <Typography variant="h3" align="center" gutterBottom>
-        Choose your next travel destination!
-      </Typography>
-      
+      {!hasSearched && (
+        <Typography variant="h3" align="center" gutterBottom>
+          Choose your next travel destination!
+        </Typography>
+      )}
+
       <SearchInterface onSearch={handleSearch} onClear={handleClear} />
-      
+
       {hasSearched && (
         <Box sx={{ mt: 3 }}>
           <Typography variant="h5" gutterBottom>
